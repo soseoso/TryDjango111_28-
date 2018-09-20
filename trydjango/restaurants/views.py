92a630e8a8d8fa8aa445e670c970306e1fc3f03b
Rhans Han <kizan9523@gmail.com>
2018-09-20 17:13:18 +0900

    30 - Associate User to Data in a Class Based View

--- trydjango/restaurants/views.py
+++ trydjango/restaurants/views.py
@@ -72,3 +72,11 @@ class RestaurantCreateView(CreateView):
 	template_name = 'restaurants/form.html'
 
 	success_url = '/restaurants/'
+
+	def form_valid(self, form):
+		instance = form.save(commit=False)
+		instance.owner = self.request.user
+		# instance.save()
+		return super(RestaurantCreateView, self).form_valid(form)
+
+		
