12735e19f1cc3a8d9791fd84b9ed69408dd2377d
Rhans Han <kizan9523@gmail.com>
2018-09-20 19:38:55 +0900

    32 - LoginView

--- trydjango/trydjango/urls.py
+++ trydjango/trydjango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+
+from django.contrib.auth.views import LoginView
+
 from restaurants.views import (
     restaurant_createview, 
     restaurant_listview, 
@@ -23,9 +26,11 @@ from restaurants.views import (
     RestaurantDetailView, 
     RestaurantCreateView 
 )
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='home.html')),  
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^login/$', LoginView.as_view(), name='login'), 
     #url(r'^restaurants/', restaurant_listview),
     url(r'^restaurants/$', RestaurantListView.as_view()),
     url(r'^restaurants/create/$', RestaurantCreateView.as_view()),
