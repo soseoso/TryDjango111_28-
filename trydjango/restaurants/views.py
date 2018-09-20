d73463a21b48f15638dca3a4ea34112e9ba2990e
Rhans Han <kizan9523@gmail.com>
2018-09-20 17:22:58 +0900

    31 - Login Required to View

--- trydjango/restaurants/views.py
+++ trydjango/restaurants/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 import random
 from django.http import HttpResponse, HttpResponseRedirect
@@ -8,6 +10,7 @@ from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from .forms import RestaurantLocationCreateForm
 from .models import RestaurantLocation
 
+@login_required(login_url='/login/')
 def restaurant_createview(request):
 	form = RestaurantLocationCreateForm(request.POST or None)
 	errors = None
@@ -67,10 +70,10 @@ class RestaurantDetailView(DetailView):
 	# 	obj = get_object_or_404(RestaurantLocation, slug=slug) # pk = rest_id
 	# 	return obj
 
-class RestaurantCreateView(CreateView):
+class RestaurantCreateView(LoginRequiredMixin, CreateView):
 	form_class = RestaurantLocationCreateForm
 	template_name = 'restaurants/form.html'
-
+	login_url = '/login/'
 	success_url = '/restaurants/'
 
 	def form_valid(self, form):
--- trydjango/trydjango/settings/base.py
+++ trydjango/trydjango/settings/base.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'trydjango.urls'
-
+LOGIN_URL = '/login/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
